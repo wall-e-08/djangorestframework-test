8aa90e5d19f9c91d205974aaf8fdebf04f07c3ca
Debashis Roy Bhowmik <debashis.buet08@gmail.com>
2019-05-19 03:35:28 +0600

    drf init

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'lame_api',
+    'django_filters',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- lame_api/models.py
+++ lame_api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class TestApi(models.Model):
+    name = models.CharField(max_length=200, blank=True, default='')
+    game_category = models.CharField(max_length=200, blank=True, default='')
+    played = models.BooleanField(default=False)
+    release_date = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('name',)
+
