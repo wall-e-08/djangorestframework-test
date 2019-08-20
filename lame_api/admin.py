a36a1df322faa5042db81641512cd28e9ee77fe2
Debashis Roy Bhowmik <debashis.buet08@gmail.com>
2019-08-20 13:56:30 +0600

    removed unnecessary django app

--- lame_api/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import TestApi
-
-
-admin.site.register(TestApi)
--- lame_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LameApiConfig(AppConfig):
-    name = 'lame_api'
